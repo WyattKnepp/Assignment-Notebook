5f2d37832e00f065d49f978b9c31f935284a0998
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-07-28 12:19:25 -0500

    Set up dummy data

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	@State private var assignmentItems = [
+		AssignmentItem(course: "Computer Science", description: "Description here", dueDate: Date()),
+		
+		AssignmentItem(course: "Computer Science", description: "Description 2 here", dueDate: Date()),
+		
+		AssignmentItem(course: "Computer Science test 2", description: "Description 3 here", dueDate: Date())
+	]
+	var body: some View {
+		Text("Hello, world!")
+			.padding()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
+}
+
+struct AssignmentItem: Identifiable, Codable {
+	var id = UUID()
+	var course = String()
+	var description = String()
+	var dueDate = Date()
 }
